b81a905cf660499aa80f71cd7744efc8e54b0c75
Andriy <andriy1909@yandex.ua>
2017-02-04 01:59:33 +0200

    1

---
+++ ABShell/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ABShell
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(491, 227);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 179);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(30, 151);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 147);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 56);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 56);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "AB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(239, 376);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 228.496F;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 41.04718F;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(695, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(36, 38);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 418);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ABShell/MainForm.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ABShell
+{
+    public partial class MainForm : Form
+    {
+        private List<UserSetting> usersList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            usersList = new List<UserSetting>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "\\ABShellSetting.conf"))
+              loadSetting();
+            UserSetting locUser = usersList.Find(x => x.name == Environment.UserName.ToString());
+            if (locUser == null || !locUser.changeShell)
+            {
+                if (MessageBox.Show("Start explorer", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    Process.Start(@"C:\Windows\explorer.exe");
+                    Application.Exit();
+                }
+            }
+            SelectQuery query = new SelectQuery("Win32_UserAccount");
+            ManagementObjectSearcher users = new ManagementObjectSearcher(query);
+            foreach (ManagementObject user in users.Get())
+            {
+                UserSetting tmpUser = usersList.Find(x => x.name == user["Name"].ToString());
+                if (tmpUser == null)
+                {
+                    usersList.Add(new UserSetting() { name = user["Name"].ToString() });
+                    dataGridView1.Rows.Add(new object[] { user["Name"], false });
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(new object[] { tmpUser.name, tmpUser.changeShell });
+                }
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\WorkSpace\ABShell\ABShell\bin\Debug\ABShell.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\ABOFFICE\client\O4Client.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*RegistryKey hklm = Registry.LocalMachine;
+            RegistryKey hkSoftware = hklm.OpenSubKey("Software");
+            RegistryKey hkMicrosoft = hkSoftware.OpenSubKey("Microsoft");
+            RegistryKey hkWindowsNT = hkMicrosoft.OpenSubKey("Windows NT");
+            RegistryKey hkCurrentVersion = hkWindowsNT.OpenSubKey("CurrentVersion");
+            RegistryKey hkWinlogon = hkCurrentVersion.OpenSubKey("Winlogon", true);
+            hkWinlogon.SetValue("Shell", "ABShell.exe");
+            MessageBox.Show(hkWinlogon.GetValue("Shell").ToString());*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*RegistryKey hklm = Registry.LocalMachine;
+            RegistryKey hkSoftware = hklm.OpenSubKey("Software");
+            RegistryKey hkMicrosoft = hkSoftware.OpenSubKey("Microsoft");
+            RegistryKey hkWindowsNT = hkMicrosoft.OpenSubKey("Windows NT");
+            RegistryKey hkCurrentVersion = hkWindowsNT.OpenSubKey("CurrentVersion");
+            RegistryKey hkWinlogon = hkCurrentVersion.OpenSubKey("Winlogon", true);
+            hkWinlogon.SetValue("Shell", "explorer.exe");
+            MessageBox.Show(hkWinlogon.GetValue("Shell").ToString());*/
+        }
+
+        public void saveSetting()
+        {
+            StreamWriter swtr = new StreamWriter(Application.StartupPath + "\\ABShellSetting.conf", false);
+            try
+            {
+                XmlSerializer xmlser = new XmlSerializer(typeof(List<UserSetting>));
+                xmlser.Serialize(swtr, usersList);
+            }
+            finally
+            {
+                swtr.Close();
+            }
+        }
+        public void loadSetting()
+        {
+            StreamReader srdr = new StreamReader(Application.StartupPath + "\\ABShellSetting.conf");
+            try
+            {
+                XmlSerializer xmlser = new XmlSerializer(typeof(List<UserSetting>));
+                usersList = (List<UserSetting>)xmlser.Deserialize(srdr);
+            }
+            finally
+            {
+                srdr.Close();
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            saveSetting();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex==1 && e.RowIndex >=0)
+            {
+                UserSetting tmpUser = usersList.Find(x => x.name == dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                if (tmpUser != null)
+                {
+                    tmpUser.changeShell = (bool)dataGridView1.Rows[e.RowIndex].Cells[1].Value;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistryKey hklm = Registry.LocalMachine;
+            RegistryKey hkSoftware = hklm.OpenSubKey("Software");
+            RegistryKey hkMicrosoft = hkSoftware.OpenSubKey("Microsoft");
+            RegistryKey hkWindowsNT = hkMicrosoft.OpenSubKey("Windows NT");
+            RegistryKey hkCurrentVersion = hkWindowsNT.OpenSubKey("CurrentVersion");
+            RegistryKey hkWinlogon = hkCurrentVersion.OpenSubKey("Winlogon", true);
+            hkWinlogon.SetValue("Shell", "ABShell.exe");
+            //MessageBox.Show(hkWinlogon.GetValue("Shell").ToString()); 
+        }
+    }
+}
